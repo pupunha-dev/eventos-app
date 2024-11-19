51958fbb676db90a3e577d0c4fe286e98fdb6837
Pupunha Dev <pupunha.dev@gmail.com>
2024-11-19 11:31:07 -0300

    Logo and Template Pagina created

---
+++ src/components/template/Logo.tsx
@@ -0,0 +1,19 @@
+import { Righteous } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+const fonte = Righteous({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export default function Logo() {
+  return (
+    <Link href='/' className={`${fonte.className} flex items-center gap-2`}>
+      <Image src="./logo.svg" alt="Logo" width={50} height={50} />
+      <h1 className="flex flex-col items-center text-lg leading-5">
+        <div>CONVIT<span className="text-blue-500">E</span></div>
+        <div>DIGITAL</div>
+      </h1>
+    </Link>
+  )
+}
--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -1,3 +1,5 @@
+import Logo from "./Logo";
+
 export interface IPagina {
   children: React.ReactNode;
   className?: string;
@@ -7,7 +9,10 @@ export const Pagina = ({ children, className }: IPagina) => {
   return (
     <div className="
     flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover">
-      <main className={className}>{children}</main>
-    </div>
+      <Logo />
+      <main className={`${className} flex flex-1 flex-col justify-center py-10 bg-red-400 container`}>
+        {children}
+      </main>
+    </div >
   )
 }
